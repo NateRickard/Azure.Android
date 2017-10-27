5251638a1e13980d3c70d991bebe515b6787f686
Nate Rickard <nate.rickard@microsoft.com>
2017-10-27 15:41:05 -0400

    added Kotlin project w/ module set up and started data module

---
+++ Data/azure.android.data/src/main/java/com/microsoft/azureandroid/data/AzureData.kt
@@ -0,0 +1,13 @@
+package com.microsoft.azureandroid.data
+
+import com.microsoft.azureandroid.data.constants.ADTokenType
+
+/**
+ * Created by nater on 10/24/17.
+ */
+class AzureData {
+
+    fun setup(name: String, key: String, keyType: ADTokenType = ADTokenType.MASTER, verboseLogging: Boolean = false) {
+
+    }
+}
---
+++ Data/azure.android.data/src/main/java/com/microsoft/azureandroid/data/constants/ADTokenType.kt
@@ -0,0 +1,9 @@
+package com.microsoft.azureandroid.data.constants
+
+/**
+ * Created by nater on 10/27/17.
+ */
+enum class ADTokenType(val value: String) {
+    MASTER("master"),
+    RESOURCE("resource")
+}
