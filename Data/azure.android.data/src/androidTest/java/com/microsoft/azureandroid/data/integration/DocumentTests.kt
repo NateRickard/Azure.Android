3d07e20190b30efaafacdac4237dd9bbd5a2de0f
Nate Rickard <nate.rickard@microsoft.com>
2017-12-13 10:29:32 -0800

    doc tests

---
+++ Data/azure.android.data/src/androidTest/java/com/microsoft/azureandroid/data/integration/DocumentTests.kt
@@ -0,0 +1,192 @@
+package com.microsoft.azureandroid.data.integration
+
+import android.support.test.runner.AndroidJUnit4
+import com.microsoft.azureandroid.data.AzureData
+import com.microsoft.azureandroid.data.delete
+import com.microsoft.azureandroid.data.model.Document
+import com.microsoft.azureandroid.data.model.Query
+import com.microsoft.azureandroid.data.model.ResourceType
+import com.microsoft.azureandroid.data.refresh
+import com.microsoft.azureandroid.data.services.ResourceResponse
+import org.awaitility.Awaitility.await
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotNull
+import org.junit.Test
+import org.junit.runner.RunWith
+
+/**
+ * Created by Nate Rickard on 12/12/17.
+ * Copyright © 2017 Nate Rickard. All rights reserved.
+ */
+
+@RunWith(AndroidJUnit4::class)
+class DocumentTests : ResourceTest<Document>(ResourceType.Document, true, true) {
+
+    val customStringKey = "customStringKey"
+    val customStringValue = "customStringValue"
+    val customNumberKey = "customNumberKey"
+    val customNumberValue = 86
+
+    @Test
+    fun createDocWithInvalidIds() {
+
+        val doc1 = Document(idWith256Chars) //too long
+        var doc1Response : ResourceResponse<Document>? = null
+        var doc2Response : ResourceResponse<Document>? = null
+
+        AzureData.createDocument(doc1, collectionId, databaseId) {
+            doc1Response = it
+        }
+
+        val doc2 = Document(idWithWhitespace) //no spaces allowed!
+
+        AzureData.createDocument(doc2, collectionId, databaseId) {
+            doc2Response = it
+        }
+
+        await().until {
+            doc1Response != null && doc2Response != null
+        }
+
+        assertResponseFailure(doc1Response)
+        assertResponseFailure(doc2Response)
+    }
+
+    @Test
+    fun createDocument() {
+
+        createNewDocument()
+    }
+
+    private fun createNewDocument() : Document {
+
+        val newDocument = Document(resourceId)
+
+        newDocument[customStringKey] = customStringValue
+        newDocument[customNumberKey] = customNumberValue
+
+        AzureData.createDocument(newDocument, collectionId, databaseId) {
+            resourceResponse = it
+        }
+
+        await().forever().until {
+            resourceResponse != null
+        }
+
+        assertResponseSuccess(resourceResponse)
+        assertEquals(resourceId, resourceResponse?.resource?.id)
+
+        val createdDoc = resourceResponse!!.resource!!
+
+        assertNotNull(createdDoc[customStringKey])
+        assertNotNull(createdDoc[customNumberKey])
+        assertEquals(customStringValue, createdDoc[customStringKey])
+        assertEquals(customNumberValue, (createdDoc[customNumberKey] as Double).toInt())
+
+        return createdDoc
+    }
+
+    @Test
+    fun listDocuments() {
+
+        //ensure at least 1 doc
+        createNewDocument()
+
+        AzureData.getDocumentsAs<Document>(collectionId, databaseId) {
+            resourceListResponse = it
+        }
+
+        await().until {
+            resourceListResponse != null
+        }
+
+        assertResponseSuccess(resourceListResponse)
+        assert(resourceListResponse?.resource?.count!! > 0)
+    }
+
+    @Test
+    fun queryDocuments() {
+
+        //ensure at least 1 doc
+        createNewDocument()
+
+        val query = Query.select()
+                .from(collectionId)
+                .where(customStringKey, customStringValue)
+                .andWhere(customNumberKey, customNumberValue)
+        .orderBy("_etag", true)
+
+        AzureData.queryDocuments(collectionId, databaseId, query) {
+            resourceListResponse = it
+        }
+
+        await().forever().until {
+            resourceListResponse != null
+        }
+
+        assertResponseSuccess(resourceListResponse)
+        assert(resourceListResponse?.resource?.count!! > 0)
+    }
+
+    @Test
+    fun getDocument() {
+
+        createNewDocument()
+
+        AzureData.getDocument<Document>(resourceId, collectionId, databaseId) {
+            resourceResponse = it
+        }
+
+        await().until {
+            resourceResponse != null
+        }
+
+        assertResponseSuccess(resourceResponse)
+        assertEquals(resourceId, resourceResponse?.resource?.id)
+
+        val doc = resourceResponse!!.resource!!
+
+        assertNotNull(doc[customStringKey])
+        assertNotNull(doc[customNumberKey])
+        assertEquals(customStringValue, doc[customStringKey])
+        assertEquals(customNumberValue, (doc[customNumberKey] as Double).toInt())
+    }
+
+    @Test
+    fun refreshDocument() {
+
+        val doc = createNewDocument()
+
+        doc.refresh {
+            resourceResponse = it
+        }
+
+        await().until {
+            resourceResponse != null
+        }
+
+        assertResponseSuccess(resourceResponse)
+        assertEquals(resourceId, resourceResponse?.resource?.id)
+
+        assertNotNull(doc[customStringKey])
+        assertNotNull(doc[customNumberKey])
+        assertEquals(customStringValue, doc[customStringKey])
+        assertEquals(customNumberValue, (doc[customNumberKey] as Double).toInt())
+    }
+
+    @Test
+    fun deleteDocument() {
+
+        val doc = createNewDocument()
+
+        doc.delete {
+            dataResponse = it
+        }
+
+        await().until {
+            dataResponse != null
+        }
+
+        assertResponseSuccess(dataResponse)
+    }
+}
